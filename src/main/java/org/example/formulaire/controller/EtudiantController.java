d8bc4ff3755822bed4f7073f581056a43218983c
farba09 <pisco7155@gmail.com>
2025-07-22 09:21:26 +0200

    modifs

--- src/main/java/org/example/formulaire/controller/EtudiantController.java
+++ src/main/java/org/example/formulaire/controller/EtudiantController.java
@@ -5,9 +5,12 @@ import org.example.formulaire.service.EtudiantService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/etudiant")
@@ -27,5 +30,25 @@ public class EtudiantController {
         List<Etudiant> etudiants = etudiantService.getEtudiants();
         model.addAttribute("etudiants", etudiants);
         return "etudiants/etudiantList";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String getEtudiantById(@PathVariable("id") UUID id, Model model) {
+        Etudiant etudiant = etudiantService.getEtudiantById(id);
+        model.addAttribute("etudiant", etudiant);
+        return "etudiants/etudiantDetail";
+    }
+
+    @GetMapping("/add")
+    public String addEtudiant(Model model) {
+        Etudiant etudiant = new Etudiant();
+        model.addAttribute("etudiant", etudiant);
+        return "etudiants/etudiantForm";
+    }
+
+    @PostMapping("/add")
+    public String addEtudiant(Etudiant etudiant) {
+        etudiantService.addEtudiant(etudiant);
+        return "redirect:/etudiant/liste";
     }
 }
